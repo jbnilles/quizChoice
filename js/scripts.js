a37f22d3e1fbb528d65d551b7fc426a411bed534
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-18 09:18:19 -0700

    makCo-authored-by: Joseph Nilles <jbnilles24@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  for(let index = 5; index <= 30; index +=5) {
+    console.log(index);
+  }
+  for(let number = 7; number <=50; number += 7){
+    console.log(number);
+  }
+
+});
