3ba4a6f50348bb71e4f02735e1db3ae536d1b385
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-18 13:43:19 -0700

    will make the stress test
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
- 
- 
+  $('#warningSigns').submit(function(){
+    event.preventDefault();
+    let numWarningSigns = $("input:checkbox[name=warningSigns]:checked").length;
+    let numSymptoms = $("input:checkbox[name=healthSymptoms]:checked").length;
+    let numCoping = $("input:checkbox[name=copingMethods]:checked").length;
+    $('#warningSigns').hide();
+    //alert(numWarningSigns + ', ' + numSymptoms + ', ' + numCoping);
+    if(numWarningSigns+numSymptoms-numCoping <= 0){
+      $('#low').show();
+    }
+    else if(numWarningSigns+numSymptoms-numCoping <= 5){
+      $('#medium').show();
+    }
+    else {
+      $('#high').show();
+    }
+    
+  });
+
 
 });
