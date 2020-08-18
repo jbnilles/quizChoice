37912f7c2c39e35e14cba06bc7b73afc1ecc2473
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-18 11:12:39 -0700

    will make the initial files
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-  for(let index = 5; index <= 30; index +=5) {
-    console.log(index);
-  }
-  for(let number = 7; number <=50; number += 7){
-    console.log(number);
-  }
+ 
+ 
 
 });
